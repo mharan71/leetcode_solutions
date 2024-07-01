bdf9b6c2e226064b096ff6cf7157e9c9ca417d5e
Matthew H <harangom@oregonstate.edu>
2024-06-30 22:30:28 -0700

    alter has_duplicate function

--- duplicate_integer.py
+++ duplicate_integer.py
@@ -5,7 +5,7 @@
 #Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 
 class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
+    def has_duplicate(self, nums: List[int]) -> bool:
         seen = set()
         for num in nums:
             if num in seen:
